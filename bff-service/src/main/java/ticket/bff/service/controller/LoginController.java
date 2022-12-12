f535a756b42b85c2834cadb45e3b429d1f36f7c2
DESKTOP-QN8CGHH\willy <willy4543@gmail.com>
2022-12-13 01:00:52 +0800

    init project

---
+++ bff-service/src/main/java/ticket/bff/service/controller/LoginController.java
@@ -0,0 +1,14 @@
+package ticket.bff.service.controller;
+
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
+@Validated
+public class LoginController {
+	
+	
+
+}
